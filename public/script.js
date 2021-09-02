187eb48fd536e7d8d982a90d1b142bf9bf6ce405
Abhi97shek <abhi.kumar736@gmail.com>
2021-09-02 23:47:47 +0530

    Adding the video Stream

--- public/script.js
+++ public/script.js
@@ -1,1 +1,29 @@
-console.log("hello");
+let myvideoStream;
+
+const videoGrid =document.getElementById('video-grid');
+const myvideo = document.createElement('video');
+myvideo.muted =true;
+
+
+navigator.mediaDevices.getUserMedia({
+    video:true,
+    audio:true
+}).then(stream=>{
+
+    myvideoStream =stream;
+    addVideoStream(myvideo,stream);
+
+});
+
+
+const addVideoStream = (video,stream)=>{
+
+video.srcObject =stream;
+video.addEventListener('loadedmetadata',()=>{
+
+    video.play();
+});
+
+    videoGrid.append(video);
+
+};
