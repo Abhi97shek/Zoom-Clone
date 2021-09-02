d12b0761b4b5eac43ba8d87540a2ba6265613f1e
Abhi97shek <abhi.kumar736@gmail.com>
2021-09-02 23:35:22 +0530

    Adding Ejs and also some fucntionality

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 const express =require('express');
-const PORT = process.env.PORT || 3030 ;
+const ejs = require('ejs');
+const path =require('path');
+const { v4 : uuidv4} = require('uuid');
+const PORT = process.env.PORT || 8000 ;
 const app = express();
+
+
+// setting a View Engine or middleware
+
+app.set('view engine','ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 
 app.get('/',(req,res)=>{
 
-    res.status(200).send("Hello World");
+    res.redirect(uuidv4());
 });
 
+app.get('/:room',(req,res)=>{
 
+    res.render('room',{
+        roomid: req.params.room
+    });
+});
 
 
 app.listen(PORT,()=>{
